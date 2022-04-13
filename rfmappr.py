225fed0bcf38c54a8574f6ffa4ac8c2194409421
M Powers <mpowers@alum.mit.edu>
2022-04-13 02:48:20 -0400

    Display markers from SQLite dataset

--- rfmappr.py
+++ rfmappr.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
-import os, random
+from flask import Flask, render_template, g
+import os
+import sqlite3
 
 app = Flask(__name__)
 app.app_context().push()
@@ -8,6 +9,35 @@ GA_TRACKING_ID = os.environ.get('GA_TRACKING_ID', 'dev')
 @app.context_processor
 def inject_global_vars():
     return {'GA_TRACKING_ID': GA_TRACKING_ID}
+
+REGIONS = ['VA', 'NC', 'TN', 'SC', 'GA', 'AL', 'MS', 'LA']
+FILE_BASE = 'Roadfood_MDP_041122_GEO'
+DB_NAME = 'roadfood'
+DATABASE = os.path.join(app.root_path, 'data', f'{FILE_BASE}.sqlite')
+
+
+def get_db():
+    db = getattr(g, '_database', None)
+    if db is None:
+        db = g._database = sqlite3.connect(DATABASE)
+    return db
+
+@app.teardown_appcontext
+def close_connection(exception):
+    db = g.get('_database', None)
+    if db is not None:
+        db.close()
+
+
+def get_rows(limit=None):
+    limit_str = f' LIMIT {limit}' if limit else ''
+    cols = ['Restaurant', 'Checkmark', 'lat', 'long', 'Crossout']
+    col_string = ', '.join(cols)
+    
+    cursor = get_db().cursor()
+    cursor.execute(f'SELECT {col_string} FROM {DB_NAME}{limit_str}')
+    items = cursor.fetchall()
+    return items
 
 
 @app.route('/')
@@ -20,13 +50,12 @@ def root():
     #     }
     # ]
     
-    top, bottom, left, right = 49.382808, 24.521208, -66.945392, -124.736342
-
+    items = get_rows()  
     markers = [{
-                'lat': random.uniform(bottom, top),
-                'lon': random.uniform(left, right),
-                'popup': 'This is a marker.'
-                } for _ in range(1000)]
-    
+                'lat': item[2],
+                'lon': item[3],
+                'popup': item[0],
+                'color': "'green'" if item[1] == 'y' else "'royalblue'"
+                } for item in items if item[2] and item[4] != 'y']
     
     return render_template('index.html', markers=markers)
--- static/js/universal.js
+++ static/js/universal.js
@@ -1,5 +1,31 @@
-var map = L.map('map').setView([39.8283, -98.5795,], 4.5);
+// top, bottom, left, right = 49.382808, 24.521208, -66.945392, -124.736342
+var map = L.map('map', {zoomDelta: 0.25, zoomSnap: 0.25})
+    .fitBounds([ [49.382808, -66.945392], [24.521208, -124.736342] ])
+    .zoomIn(.25, animate=true);
+//    .setView([39.8283, -98.5795,], 4.5);
 
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
+
+
+// https://www.section.io/engineering-education/how-to-build-a-real-time-location-tracker-using-leaflet-js/
+/*
+function getPosition(position) {
+  console.log(position)
+  lat = position.coords.latitude;
+  long = position.coords.longitude;
+  accuracy = position.coords.accuracy;
+
+  console.log(
+    "Your coordinate is: Lat: " +
+      lat +
+      " Long: " +
+      long +
+      " Accuracy: " +
+      accuracy
+  );
+}
+
+navigator.geolocation.getCurrentPosition(getPosition);
+*/
