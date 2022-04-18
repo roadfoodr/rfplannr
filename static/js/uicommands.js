b5401657d8deeecddc88cae3787e53dfa0b4f2ca
M Powers <mpowers@alum.mit.edu>
2022-04-18 01:00:30 -0400

    Add click handler to toggle selection status of marker

--- static/js/uicommands.js
+++ static/js/uicommands.js
@@ -4,12 +4,16 @@ map
     .on('keypress', function(e){keypress(e);});
 
 
+const keyHash = 'H';
+const colorSelected = 'crimson';
+
+
 function keypress(e) {
     console.log(e.originalEvent);
     var key = e.originalEvent.key;
     console.log(key);
 
-    if (key == 'H') {
+    if (key == keyHash) {
         var hashids = new Hashids();
 //        console.log(hashids.encode(347, 1, 2, 3, 4));
         
@@ -24,10 +28,21 @@ function keypress(e) {
         }
 }
 
-
-
-/*    
-    if (event.originalEvent.ctrlKey) {
-    // handle ctrl + click ...
-
-*/
+function clickselect(e) {
+    console.log(e);
+    marker = e.target;
+    if (marker.options.color == colorSelected) {
+        marker.setStyle({
+            color: marker.options.originalColor,
+            fillColor: marker.options.originalColor,
+        });
+        marker.options.selected = 'no';
+    }
+    else{
+        marker.setStyle({
+            color: colorSelected,
+            fillColor: colorSelected,
+        });
+        marker.options.selected = 'yes';
+    }
+}
