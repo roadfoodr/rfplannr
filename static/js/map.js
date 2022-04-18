9ad278521248640727821aaf8db825ee4ecef6c1
M Powers <mpowers@alum.mit.edu>
2022-04-18 01:25:37 -0400

    Add selectable map layer

--- static/js/map.js
+++ static/js/map.js
@@ -3,12 +3,15 @@ var map = L.map('map', {zoomDelta: 0.25, zoomSnap: .1})
     .fitBounds([ [49.382808, -66.945392], [24.521208, -124.736342] ]);
 //    .setView([39.8283, -98.5795,], 4.5);
 
-var tileLayer = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+var OpenStreetMap = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-
-
+var CartoDB_Voyager = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager/{z}/{x}/{y}{r}.png', {
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
+	subdomains: 'abcd',
+	maxZoom: 20
+}).addTo(map);
 
 
 
